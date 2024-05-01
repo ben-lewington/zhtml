993163b721af0fb7d59275291b996de133844f92
Ben Lewington <ben.lewington1991@gmail.com>
2024-05-01 19:07:21 +0100

    maybe if the stack actually worked I wouldnt keep segfaulting.added a test too

--- src/big_enough.zig
+++ src/big_enough.zig
@@ -1,4 +1,5 @@
-const assert = @import("std").debug.assert;
+const std = @import("std");
+const assert = std.debug.assert;
 
 // These are fixed size dynamic arrays. Make sure that inner is big enough for your puposes,
 // else it's going to crash!
@@ -26,14 +27,6 @@ pub fn Buffer(comptime T: type) type {
             for (slice, 0..) |p, i| self.inner[self.len + i] = p;
             self.len += slice.len;
         }
-
-        pub inline fn extend_until(self: *Self, slice: []const T) usize {
-            var i = 0;
-            while (self.len + i < self.inner.len) : (i += 1) {
-                self.inner[self.len + i] = slice[i];
-            }
-            return i;
-        }
     };
 }
 
@@ -41,7 +34,7 @@ pub fn Stack(comptime T: type) type {
     return struct {
         const Self = @This();
         inner: []T,
-        len: ?usize = null,
+        top: ?usize = null,
 
         pub inline fn init(inner: []T) Self {
             return .{
@@ -50,31 +43,55 @@ pub fn Stack(comptime T: type) type {
         }
 
         pub inline fn push(self: *Self, item: T) void {
-            assert(self.len orelse 0 < self.inner.len);
-            // @import("std").log.debug("pushing: {*}", .{item});
-            const len = self.len orelse 0;
-            self.inner[len] = item;
-            if (self.len) |l| {
-                self.len = l + 1;
-            } else self.len = 0;
+            assert(self.top orelse 0 < self.inner.len);
+            if (self.top) |t| {
+                self.inner[t + 1] = item;
+                self.top = t + 1;
+            } else {
+                self.inner[0] = item;
+                self.top = 0;
+            }
         }
 
         pub inline fn pop(self: *Self) T {
-            assert(if (self.len) |_| true else false);
-            if (self.len) |l| {
-                defer {
-                    if (l == 0) {
-                        self.len = null;
-                    } else {
-                        self.len = l - 1;
-                    }
+            assert(if (self.top) |_| true else false);
+            var ret: T = undefined;
+            if (self.top) |t| {
+                if (t > 0) {
+                    self.top = t - 1;
+                    ret = self.inner[t];
+                } else {
+                    self.top = null;
+                    ret = self.inner[0];
                 }
-                return self.inner[l];
             } else unreachable;
+            return ret;
         }
 
         pub inline fn peek(self: *const Self) ?T {
-            return if (self.len) |l| self.inner[l] else null;
+            if (self.top) |t| {
+                const item = self.inner[t];
+                return item;
+            } else return null;
         }
     };
+}
+
+test "stack works" {
+    const alc = std.testing.allocator;
+    const buf = try alc.alloc(usize, 50);
+    defer alc.free(buf);
+
+    var stack = Stack(usize).init(buf);
+    try std.testing.expectEqual(null, stack.peek());
+
+    stack.push(1);
+    const t = stack.pop();
+    try std.testing.expectEqual(1, t);
+
+    stack.push(2);
+    try std.testing.expectEqualSlices(usize, &[_]usize{2}, stack.inner[0 .. stack.top.? + 1]);
+    stack.push(3);
+    try std.testing.expectEqualSlices(usize, &[_]usize{ 2, 3 }, stack.inner[0 .. stack.top.? + 1]);
+    try std.testing.expectEqual(3, stack.peek());
 }
